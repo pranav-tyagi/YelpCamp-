ca570f62613c16ce22c055e0441c8fc7e07b0283
pranav-tyagi <54205928+pranav-tyagi@users.noreply.github.com>
2020-01-23 19:35:48 +0530

    Add files via upload

---
+++ YelpCamp2/seed.js
@@ -0,0 +1,53 @@
+var mongoose=require("mongoose");
+var Campground=require("./models/campground");
+var Comment=require("./models/comment");
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
+function seedDB(){
+    Campground.remove({},function(err,camp){
+        if(err){
+            console.log(err);
+        }
+        console.log("data removed");
+        data.forEach(function(seed){
+            Campground.create(seed,function(err,camp){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("data added");
+                    Comment.create({
+                        text:"this is a cool place to visit.",
+                        author:"pranav tyagi"
+                    },function(err,comment){
+                        if(err){
+                            console.log(err);
+                        }
+                        else{
+                            camp.comments.push(comment);
+                            camp.save();
+                            console.log("comment added");
+                        }
+                    });
+                }
+            });
+        });
+    });
+    
+}
+module.exports=seedDB;
